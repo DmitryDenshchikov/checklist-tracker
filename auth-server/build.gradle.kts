d15d706ea5984c5db6ca70e324f61ab705fa01e3
Dmitry <mr.dd33@yandex.ru>
2023-10-16 21:43:36 +0300

    Simplify project structure (#6)
    * Remove buildSrc
    
    * Fix comments

--- auth-server/build.gradle.kts
+++ auth-server/build.gradle.kts
@@ -1,17 +1,37 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("denshchikov.dmitry.kotlin-application-conventions")
+    val kotlinVersion = "1.9.10"
+
+    // gradle
+    `kotlin-dsl`
+
+    // jetbrains
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("jvm") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
+
+    // spring
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
 }
 
-dependencies {
-    val psqlVersion: String by project
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
 
+repositories {
+    mavenCentral()
+}
+
+val postgresqlVersion by extra("42.6.0")
+
+dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.security:spring-security-oauth2-authorization-server")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
-    implementation("org.postgresql:postgresql:$psqlVersion")
+    implementation("org.postgresql:postgresql:$postgresqlVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
@@ -25,8 +45,8 @@ tasks.withType<KotlinCompile> {
     }
 }
 
-application {
-    mainClass.set("denshchikov.dmitry.app.AppKt")
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 tasks.getByName<Jar>("jar") {
