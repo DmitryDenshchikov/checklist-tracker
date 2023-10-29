d2a92a32562d31f9d1f89ae9ca3a454ea80f3ae2
Dmitry Denshchikov <mr.dd33@yandex.ru>
2023-10-29 18:47:00 +0200

    Disable SSL for simplifying k8s deployments testing

--- app/src/main/kotlin/denshchikov/dmitry/app/config/SecurityConfig.kt
+++ app/src/main/kotlin/denshchikov/dmitry/app/config/SecurityConfig.kt
@@ -18,11 +18,11 @@ class SecurityConfig {
             it.disable()
         }.authorizeHttpRequests {
             it
-                .requestMatchers(PUT, "/tasks/**").hasAuthority("SCOPE_tasks.update")
-                .requestMatchers(POST, "/tasks").hasAuthority("SCOPE_tasks.create")
-                .requestMatchers(GET, "/tasks").hasAuthority("SCOPE_tasks.read")
-                .requestMatchers(GET, "/tasks/**").hasAuthority("SCOPE_tasks.read")
-                .requestMatchers( "/ws/notifications").hasAuthority("SCOPE_notifications.read")
+                .requestMatchers(PUT, "/tasks/**").hasAuthority("SCOPE_user")
+                .requestMatchers(POST, "/tasks").hasAuthority("SCOPE_user")
+                .requestMatchers(GET, "/tasks").hasAuthority("SCOPE_user")
+                .requestMatchers(GET, "/tasks/**").hasAuthority("SCOPE_user")
+                .requestMatchers( "/ws/notifications").hasAuthority("SCOPE_user")
         }.oauth2ResourceServer {
             it.jwt(withDefaults())
         }
